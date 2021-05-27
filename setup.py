403a376cba8ed7fd92073557aff8d1402c3d10a3
Yossi Rozantsev <54272821+Apakottur@users.noreply.github.com>
2021-05-27 16:39:17 +0000

    Fix __init__ file (#15)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-version = "0.0.7"
+version = "0.0.8"
 
 setuptools.setup(
     name="shpyx",
--- shpyx/__init__.py
+++ shpyx/__init__.py
@@ -1,3 +1,14 @@
+__all__ = [
+    # From .cmd_runner
+    "ShellCmdRunner",
+    "run",
+    # From .errors
+    "InternalError",
+    "VerificationError",
+    # From .result
+    "ShellCmdResult",
+]
+
 from .cmd_runner import ShellCmdRunner, run
 from .errors import InternalError, VerificationError
 from .result import ShellCmdResult
