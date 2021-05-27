6cc97f5f0f71a24447b8debb4a74d908e48090d6
yossi <54272821+Apakottur@users.noreply.github.com>
2021-05-27 01:12:36 +0000

    Cleanup code and add docstrings

---
+++ shpyx/cmd_runner.py
@@ -0,0 +1,235 @@
+import fcntl
+import os
+import subprocess
+import sys
+import time
+from typing import Optional
+
+from shpyx.errors import InternalError, VerificationError
+from shpyx.result import ShellCmdResult
+
+
+def _is_action_required(user_value: Optional[bool], default_value: bool):
+    """
+    Returns whether an action needs to be done, based on whether the user required it and the default value of the
+    runner.
+    """
+    if user_value is True:
+        # User required the action.
+        return True
+    elif user_value is False:
+        # User required the action not to be taken.
+        return False
+    else:
+        # Use did not define whether the action needs to be done, use the default.
+        return default_value
+
+
+class ShellCmdRunner:
+    def __init__(
+        self,
+        log_cmd: bool = False,
+        log_output: bool = False,
+        verify_return_code: bool = True,
+        verify_stderr: bool = False,
+    ) -> None:
+        """
+        Create a shell command runner.
+
+        The parameters given to the constructor define the default behavior of the subprocess which runs the shell
+        command. Most of them can be overridden by the call to `.run`.
+
+        :param log_cmd: Whether to log the executed command.
+        :param log_output: Whether to log the live output of the command (while it is being executed).
+
+        :param verify_return_code: Whether to raise an exception if the shell return code of the command is not `0`.
+        :param verify_stderr: Whether to raise an exception if the command wrote anything to stderr.
+        """
+        self._log_cmd: bool = log_cmd
+        self._log_output: bool = log_output
+        self._verify_return_code: bool = verify_return_code
+        self._verify_stderr: bool = verify_stderr
+
+    @staticmethod
+    def _log(msg: str) -> None:
+        """
+        Log a message to the standard output.
+        """
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+    def _maybe_log_cmd(self, cmd: str, log_cmd: Optional[bool]) -> None:
+        """
+        Log the command, if required by the user or by the default behavior.
+        :param cmd: The command.
+        :param log_cmd: Whether to log the command, as supplied to `.run`.
+        """
+        if _is_action_required(log_cmd, self._log_cmd):
+            self._log(f"Running: {cmd}\n")
+
+    def _add_stdout(self, result: ShellCmdResult, data: str, log_output: Optional[bool]) -> None:
+        """
+        Add partial stdout output to the result.
+        :param result: The result object of the command.
+        :param data: The partial stdout output to add.
+        :param log_output: Whether to log the output, as supplied to `.run`.
+        """
+        if "" == data:
+            return
+
+        result.stdout += data
+        result.all_output += data
+
+        if _is_action_required(log_output, self._log_output):
+            self._log(data)
+
+    def _add_stderr(self, result: ShellCmdResult, data: str, log_output: Optional[bool]) -> None:
+        """
+        Add partial stderr output to the result.
+        :param result: The result object of the command.
+        :param data: The partial stdout output to add.
+        :param log_output: Whether to log the output, as supplied to `.run`.
+        """
+        if "" == data:
+            return
+
+        result.stderr += data
+        result.all_output += data
+
+        if _is_action_required(log_output, self._log_output):
+            self._log(data)
+
+    def _verify_result(
+        self, result: ShellCmdResult, verify_return_code: Optional[bool], verify_stderr: Optional[bool]
+    ) -> None:
+        """
+        Verify that the shell command executed successfully.
+        The success is defined by a set of tests on the command outputs.
+
+        :param result: The command result object.
+        :param verify_return_code: Whether to verify that the return code is `0`.
+        :param verify_stderr: Whether to verify that the command did not write to `stderr`.
+        """
+        success = True
+
+        if _is_action_required(verify_return_code, self._verify_return_code):
+            success &= result.return_code == 0
+
+        if _is_action_required(verify_stderr, self._verify_stderr):
+            success &= result.stderr == ""
+
+        if not success:
+            raise VerificationError(
+                f"The command '{result.cmd}' failed with return code {result.return_code}.\n\n"
+                f"Error output:\n{result.stderr}\nAll output:\n{result.all_output}",
+                result=result,
+            )
+
+    def run(
+        self,
+        cmd: str,
+        *,
+        log_cmd: Optional[bool] = False,
+        log_output: Optional[bool] = False,
+        verify_return_code: Optional[bool] = True,
+        verify_stderr: Optional[bool] = False,
+        env: Optional[dict[str, str]] = None,
+        exec_dir: Optional[str] = None,
+    ) -> ShellCmdResult:
+        """
+        Run the given shell command.
+        This is the core of `shpyX`.
+
+        :param cmd: The shell command to run in a subprocess.
+
+        :param log_cmd: Whether to log the executed command.
+        :param log_output: Whether to log the live output of the command (while it is being executed).
+
+        :param verify_return_code: Whether to raise an exception if the shell return code of the command is not `0`.
+        :param verify_stderr: Whether to raise an exception if the command wrote anything to stderr.
+
+        :param env: Environment variables to set during the execution of the command (in addition to those of the parent
+                    process, which will also be available to the subprocess).
+
+        :param exec_dir: Path where the command will be executed.
+
+        :return: The result, as a `ShellCmdResult` object.
+        """
+
+        # Log the command, if required.
+        self._maybe_log_cmd(cmd, log_cmd)
+
+        # Build the command environment variables.
+        cmd_env = os.environ.copy()
+        if env is not None:
+            cmd_env = os.environ.copy() | env
+
+        # Initialize the subprocess wrapper.
+        p = subprocess.Popen(
+            [cmd],
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=cmd_env,
+            cwd=exec_dir,
+        )
+
+        # Verify that all the pipes were properly configured.
+        if not (p and p.stdout and p.stderr):
+            raise InternalError("Failed to initialize subprocess.")
+
+        # Initialize the result object.
+        result = ShellCmdResult(cmd=cmd)
+
+        # Make all the command outputs non blocking, so that it can be interrupted.
+        fcntl.fcntl(p.stdout.fileno(), fcntl.F_SETFL, fcntl.fcntl(p.stdout.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK)
+        fcntl.fcntl(p.stderr.fileno(), fcntl.F_SETFL, fcntl.fcntl(p.stderr.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK)
+
+        # Run the command.
+        # The command runs in a subprocess and it's outputs are periodically checked and added to the result object.
+        while p.poll() is None:
+            stdout_data = ""
+            stderr_data = ""
+
+            # Poll both outputs for any new data.
+            try:
+                stdout_data = p.stdout.read()
+            except TypeError:
+                pass
+            try:
+                stderr_data = p.stdout.read()
+            except TypeError:
+                pass
+
+            # Update the result object and log the outputs, if needed.
+            self._add_stdout(result, stdout_data, log_output)
+            self._add_stderr(result, stderr_data, log_output)
+
+            time.sleep(0.1)
+
+        # Get the remaining outputs and add them to the result.
+        final_stdout, final_stderr = p.communicate()
+        self._add_stdout(result, final_stdout, log_output)
+        self._add_stderr(result, final_stderr, log_output)
+
+        p.stdout.close()
+        p.stderr.close()
+
+        result.return_code = p.returncode
+
+        # Verify that the command result is valid, based on the verification configuration.
+        self._verify_result(result, verify_return_code, verify_stderr)
+
+        return result
+
+
+# A runner object with default configuration.
+_default_runner = ShellCmdRunner()
+
+# The default run function, which can be used with `shpyx.run`.
+run = _default_runner.run
+
+# Aliases of `run`.
+ex = run
+execute = run
---
+++ shpyx/result.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ShellCmdResult:
+    """
+    The result of an execution of a shell command.
+    """
+
+    # The command that was run in the shell.
+    cmd: str
+
+    # The output streams of the command: Standard Output and Standard Error.
+    stdout: str = ""
+    stderr: str = ""
+
+    # All the output of the command (stdout + stderr) as it would have appeared on screen.
+    # Note that this is NOT necessarily equal to `self.stdout + self.stderr`,
+    # as the two streams are written in parallel.
+    all_output: str = ""
+
+    # The return code of the command.
+    # Apart from 0 meaning success (which is also not always the case), there is no general mapping of return codes
+    # to errors.
+    return_code: int = -1
